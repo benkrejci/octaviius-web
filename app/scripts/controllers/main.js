3cd9ca8c641f8088d95890ed678ff2fa16985dcb
ben <benkrejci@gmail.com>
2016-07-12 17:34:01 -0400

    initial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name octaviiusWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the octaviiusWebApp
+ */
+angular.module('octaviiusWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
